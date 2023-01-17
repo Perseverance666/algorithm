4a4f419332f588fa7b966895c18747743ff240f9
Perseverance <1445272085@qq.com>
2023-01-17 22:19:52 +0800

    day14

---
+++ algorithm-basic/src/main/java/com/example/class14/Code05_Light.java
@@ -0,0 +1,112 @@
+package com.example.class14;
+
+import java.util.HashSet;
+
+/**
+ * @Date: 2023/1/17 21:23
+ */
+public class Code05_Light {
+    public static int minLight1(String road) {
+        if (road == null || road.length() == 0) {
+            return 0;
+        }
+        return process(road.toCharArray(), 0, new HashSet<>());
+    }
+
+    // str[index....]位置，自由选择放灯还是不放灯
+    // str[0..index-1]位置呢？已经做完决定了，那些放了灯的位置，存在lights里
+    // 要求选出能照亮所有.的方案，并且在这些有效的方案中，返回最少需要几个灯
+    public static int process(char[] str, int index, HashSet<Integer> lights) {
+        if (index == str.length) { // 结束的时候
+            for (int i = 0; i < str.length; i++) {
+                if (str[i] != 'X') { // 当前位置是点的话
+                    if (!lights.contains(i - 1) && !lights.contains(i) && !lights.contains(i + 1)) {
+                        return Integer.MAX_VALUE;
+                    }
+                }
+            }
+            return lights.size();
+        } else { // str还没结束
+            // i X .
+            int no = process(str, index + 1, lights);
+            int yes = Integer.MAX_VALUE;
+            if (str[index] == '.') {
+                lights.add(index);
+                yes = process(str, index + 1, lights);
+                lights.remove(index);
+            }
+            return Math.min(no, yes);
+        }
+    }
+
+    public static int minLight2(String road) {
+        char[] str = road.toCharArray();
+        int i = 0;
+        int light = 0;
+        while (i < str.length) {
+            //1、如果i位置是X不用放灯，去看下一个位置
+            if (str[i] == 'X') {
+                i++;
+            } else {
+                //2、i位置是点，无论越界，i+1,i+2上是点还是X，都要放灯
+                light++;
+                if (i + 1 == str.length) {
+                    //下一个位置越界，结束循环
+                    break;
+                } else {
+                    if (str[i + 1] == 'X') {
+                        //3、i位置是点，i+1位置是X，i上放灯，然后去看i+2位置
+                        i = i + 2;
+                    } else {
+                        //4、i位置是点，i+1位置是点，无论i+2是点还是X，i+1位置都要放灯(i+2是X时，i+1和i哪里放灯都行)，然后去看i+3位置
+                        i = i + 3;
+                    }
+                }
+            }
+        }
+        return light;
+    }
+
+    // 更简洁的解法
+    // 两个X之间，数一下.的数量，然后除以3，向上取整
+    // 把灯数累加
+    public static int minLight3(String road) {
+        char[] str = road.toCharArray();
+        int cur = 0;
+        int light = 0;
+        for (char c : str) {
+            if (c == 'X') {
+                light += (cur + 2) / 3;
+                cur = 0;
+            } else {
+                cur++;
+            }
+        }
+        light += (cur + 2) / 3;
+        return light;
+    }
+
+    // for test
+    public static String randomString(int len) {
+        char[] res = new char[(int) (Math.random() * len) + 1];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = Math.random() < 0.5 ? 'X' : '.';
+        }
+        return String.valueOf(res);
+    }
+
+    public static void main(String[] args) {
+        int len = 20;
+        int testTime = 100000;
+        for (int i = 0; i < testTime; i++) {
+            String test = randomString(len);
+            int ans1 = minLight1(test);
+            int ans2 = minLight2(test);
+            int ans3 = minLight3(test);
+            if (ans1 != ans2 || ans1 != ans3) {
+                System.out.println("oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+}
