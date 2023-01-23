28d936fd0689353b2f660896a42c71d0f3349293
Perseverance <1445272085@qq.com>
2023-01-23 16:41:14 +0800

    day15

---
+++ algorithm-basic/src/main/java/com/example/class15/Code02_FriendCircles.java
@@ -0,0 +1,85 @@
+package com.example.class15;
+
+/**
+ * @Date: 2023/1/20 19:42
+ * 测试链接：https://leetcode.cn/problems/number-of-provinces/
+ *
+ * 省份数量
+ * 题目：有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+ * 省份是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+ * 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
+ * 返回矩阵中 省份 的数量
+ */
+public class Code02_FriendCircles {
+    public static int findCircleNum(int[][] M) {
+        int N = M.length;
+        // {0} {1} {2} {N-1}
+        UnionFind unionFind = new UnionFind(N);
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (M[i][j] == 1) { // i和j互相认识
+                    unionFind.union(i, j);
+                }
+            }
+        }
+        return unionFind.sets();
+    }
+
+    public static class UnionFind {
+        // parent[i] = k ： i的父亲是k
+        private int[] parent;
+        // size[i] = k ： 如果i是代表节点，size[i]才有意义，否则无意义
+        // i所在的集合大小是多少
+        private int[] size;
+        // 辅助结构
+        private int[] help;
+        // 一共有多少个集合
+        private int sets;
+
+        public UnionFind(int N) {
+            parent = new int[N];
+            size = new int[N];
+            help = new int[N];
+            sets = N;
+            for (int i = 0; i < N; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        // 查
+        // 从i开始一直往上，往上到不能再往上，代表节点，返回
+        // 这个过程要做路径压缩
+        private int find(int i) {
+            int hi = 0;
+            while (i != parent[i]) {
+                help[hi++] = i;
+                i = parent[i];
+            }
+            for (hi--; hi >= 0; hi--) {
+                parent[help[hi]] = i;
+            }
+            return i;
+        }
+
+        //并
+        public void union(int i, int j) {
+            int f1 = find(i);
+            int f2 = find(j);
+            if (f1 != f2) {
+                if (size[f1] >= size[f2]) {
+                    size[f1] += size[f2];
+                    parent[f2] = f1;
+                } else {
+                    size[f2] += size[f1];
+                    parent[f1] = f2;
+                }
+                sets--;
+            }
+        }
+
+        public int sets() {
+            return sets;
+        }
+    }
+}
