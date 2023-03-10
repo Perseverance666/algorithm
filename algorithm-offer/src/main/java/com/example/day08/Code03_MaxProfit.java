ba0e51987d3899b45c89a483c4ed404377c8889b
Perseverance <1445272085@qq.com>
2023-03-10 20:36:50 +0800

    剑指offer_day08

---
+++ algorithm-offer/src/main/java/com/example/day08/Code03_MaxProfit.java
@@ -0,0 +1,20 @@
+package com.example.day08;
+
+/**
+ * @Date: 2023/3/10 20:19
+ * 剑指 Offer 63. 股票的最大利润
+ */
+public class Code03_MaxProfit {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1){
+            return 0;
+        }
+        int cost = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            cost = Math.min(cost,prices[i]);
+            profit = Math.max(profit,prices[i] - cost);
+        }
+        return profit;
+    }
+}
