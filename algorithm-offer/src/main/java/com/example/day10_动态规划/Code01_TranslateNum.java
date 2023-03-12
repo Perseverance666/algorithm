3c43fddc148aca9009fa072d64261b9aa1e90596
Perseverance <1445272085@qq.com>
2023-03-12 20:09:00 +0800

    剑指offer_day10

--- algorithm-offer/src/main/java/com/example/day10_动态规划/Code01_TranslateNum.java
+++ algorithm-offer/src/main/java/com/example/day10_动态规划/Code01_TranslateNum.java
@@ -6,6 +6,26 @@ package com.example.day10_动态规划;
  */
 public class Code01_TranslateNum {
     public int translateNum(int num) {
-        return 1;
+        char[] str = String.valueOf(num).toCharArray();//将num转为字符串
+        if (str.length == 1) {
+            return 1;
+        }
+        //dp[i]表示str[0]~str[i]结尾的翻译方法数
+        int[] dp = new int[str.length];
+        dp[0] = 1;
+        if (str[0] - '0' > 2 || str[0] - '0' == 0 || (str[0] - '0' == 2 && str[1] - '0' > 5)) {
+            dp[1] = 1;
+        } else {
+            dp[1] = 2;
+        }
+        for (int i = 2; i < str.length; i++) {
+            if (str[i - 1] - '0' > 2 || str[i - 1] - '0' == 0 || (str[i - 1] - '0' == 2 && str[i] - '0' > 5)) {
+                dp[i] = dp[i - 1];
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+        }
+        return dp[str.length - 1];
     }
 }
--- algorithm-offer/src/main/java/com/example/day10_动态规划/Code02_LengthOfLongestSubstring.java
+++ algorithm-offer/src/main/java/com/example/day10_动态规划/Code02_LengthOfLongestSubstring.java
@@ -1,11 +1,72 @@
 package com.example.day10_动态规划;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Date: 2023/3/12 12:57
  * 剑指 Offer 48. 最长不含重复字符的子字符串
  */
 public class Code02_LengthOfLongestSubstring {
+    //将HashMap改成数组类型，能优化时间
     public int lengthOfLongestSubstring(String s) {
-        return 1;
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        //map记录key这个字符最后一次出现的位置
+        int[] map = new int[256];
+        for (int i = 0; i < 256; i++) {
+            map[i] = -1;
+        }
+        map[str[0]] = 0;
+        int res = 1;
+        //dp[i]表示以str[i]为结尾的最长子字符串的长度
+        int[] dp = new int[str.length];
+        dp[0] = 1;
+        for (int i = 1; i < str.length; i++) {
+            if (map[str[i]] == -1) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                Integer preI = map[str[i]];
+                //1、preI ~ i 之间没有出现重复的字母
+                int p1 = i - preI;
+                //2、preI ~ i 之间出现了重复的字母
+                int p2 = dp[i] = dp[i - 1] + 1;
+                dp[i] = Math.min(p1, p2);
+            }
+            map[str[i]] = i;
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
+
+//    public int lengthOfLongestSubstring(String s) {
+//        if (s == null || s.length() == 0) {
+//            return 0;
+//        }
+//        char[] str = s.toCharArray();
+//        //map记录key这个字符最后一次出现的位置
+//        Map<Character, Integer> map = new HashMap<>();
+//        map.put(str[0], 0);
+//        int res = 1;
+//        //dp[i]表示以str[i]为结尾的最长子字符串的长度
+//        int[] dp = new int[str.length];
+//        dp[0] = 1;
+//        for (int i = 1; i < str.length; i++) {
+//            if (!map.containsKey(str[i])) {
+//                dp[i] = dp[i - 1] + 1;
+//            } else {
+//                Integer preI = map.get(str[i]);
+//                //1、preI ~ i 之间没有出现重复的字母
+//                int p1 = i - preI;
+//                //2、preI ~ i 之间出现了重复的字母
+//                int p2 = dp[i] = dp[i - 1] + 1;
+//                dp[i] = Math.min(p1, p2);
+//            }
+//            map.put(str[i], i);
+//            res = Math.max(res, dp[i]);
+//        }
+//        return res;
+//    }
 }
